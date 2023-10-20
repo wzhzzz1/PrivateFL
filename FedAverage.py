52bf060809e7dfc45646749a326617596509ecff
wangzihang <1013054127@qq.com>
2023-10-20 19:51:08 +0800

    添加数据加载部分注释

--- FedAverage.py
+++ FedAverage.py
@@ -8,6 +8,7 @@ import time
 
 start_time = time.time()
 
+
 def parse_arguments():
     '''
     data:数据集，默认mnist
@@ -26,48 +27,52 @@ def parse_arguments():
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='mnist',
-                        choices=['mnist','cifar10','cifar100','fashionmnist','emnist','purchase','chmnist'])
-    parser.add_argument('--nclient', type=int, default= 100)
-    parser.add_argument('--nclass', type=int, help= 'the number of class for this dataset', default= 10)
-    parser.add_argument('--ncpc', type=int, help= 'the number of class assigned to each client', default=2)
-    parser.add_argument('--model', type=str, default='mnist_fully_connected_IN', choices = ['mnist_fully_connected_IN', 'resnet18_IN', 'alexnet_IN', 'purchase_fully_connected_IN', 'mnist_fully_connected', 'resnet18', 'alexnet', 'purchase_fully_connected'])
-    parser.add_argument('--mode', type=str, default= 'LDP')
-    parser.add_argument('--round',  type = int, default= 150)
+                        choices=['mnist', 'cifar10', 'cifar100', 'fashionmnist', 'emnist', 'purchase', 'chmnist'])
+    parser.add_argument('--nclient', type=int, default=100)
+    parser.add_argument('--nclass', type=int, help='the number of class for this dataset', default=10)
+    parser.add_argument('--ncpc', type=int, help='the number of class assigned to each client', default=2)
+    parser.add_argument('--model', type=str, default='mnist_fully_connected_IN',
+                        choices=['mnist_fully_connected_IN', 'resnet18_IN', 'alexnet_IN', 'purchase_fully_connected_IN',
+                                 'mnist_fully_connected', 'resnet18', 'alexnet', 'purchase_fully_connected'])
+    parser.add_argument('--mode', type=str, default='LDP')
+    parser.add_argument('--round', type=int, default=150)
     parser.add_argument('--epsilon', type=int, default=8)
-    parser.add_argument('--physical_bs', type = int, default=3, help= 'the max_physical_batch_size of Opacus LDP, decrease if cuda out of memory')
-    parser.add_argument('--sr',  type=float, default=1.0,
+    parser.add_argument('--physical_bs', type=int, default=3,
+                        help='the max_physical_batch_size of Opacus LDP, decrease if cuda out of memory')
+    parser.add_argument('--sr', type=float, default=1.0,
                         help='sample rate in each round')
-    parser.add_argument('--lr',  type=float, default=1e-1,
+    parser.add_argument('--lr', type=float, default=1e-1,
                         help='learning rate')
-    parser.add_argument('--flr',  type=float, default=1e-1,
+    parser.add_argument('--flr', type=float, default=1e-1,
                         help='learning rate')
-    parser.add_argument('--E',  type=int, default=1,
+    parser.add_argument('--E', type=int, default=1,
                         help='the index of experiment in AE')
     args = parser.parse_args()
     return args
 
+
 args = parse_arguments()
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  #判断GPU是否可用
-today = date.today().isoformat()   #返回一个表示当前本地日期的date对象，并将格式改为'YYYY-MM-DD'的字符串
-DATA_NAME = args.data   #数据集名称
-NUM_CLIENTS = args.nclient  #客户端的数量
-NUM_CLASSES = args.nclass  #数据集标签的数量
-NUM_CLASES_PER_CLIENT= args.ncpc   #分给每个客户端标签的数量
-MODEL = args.model   #训练使用的模型
-MODE = args.mode   #判断是CDP还是LDP
-EPOCHS = 1  #本地训练轮次
-ROUNDS = args.round   #全局训练轮次
-BATCH_SIZE = 64  #本地每次训练的批量大小
-LEARNING_RATE_DIS = args.lr  #学习率
-LEARNING_RATE_F = args.flr  #不知道干嘛的
-mp_bs = args.physical_bs  #不知道干嘛的
-target_epsilon = args.epsilon  #隐私预算
-target_delta = 1e-3  #差分隐私高斯机制的delta
-sample_rate=args.sr  #采样率
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # 判断GPU是否可用
+today = date.today().isoformat()  # 返回一个表示当前本地日期的date对象，并将格式改为'YYYY-MM-DD'的字符串
+DATA_NAME = args.data  # 数据集名称
+NUM_CLIENTS = args.nclient  # 客户端的数量
+NUM_CLASSES = args.nclass  # 数据集标签的数量
+NUM_CLASES_PER_CLIENT = args.ncpc  # 分给每个客户端标签的数量
+MODEL = args.model  # 训练使用的模型
+MODE = args.mode  # 判断是CDP还是LDP
+EPOCHS = 1  # 本地训练轮次
+ROUNDS = args.round  # 全局训练轮次
+BATCH_SIZE = 64  # 本地每次训练的批量大小
+LEARNING_RATE_DIS = args.lr  # 学习率
+LEARNING_RATE_F = args.flr  # 不知道干嘛的
+mp_bs = args.physical_bs  # 不知道干嘛的
+target_epsilon = args.epsilon  # 隐私预算
+target_delta = 1e-3  # 差分隐私高斯机制的delta
+sample_rate = args.sr  # 采样率
 
-os.makedirs(f'log/E{args.E}', exist_ok=True)   #判断EX文件存不存在，不存在就创建
-user_param = {'disc_lr': LEARNING_RATE_DIS, 'epochs': EPOCHS}   #参数字典，包含了学习率以及本地训练轮次
+os.makedirs(f'log/E{args.E}', exist_ok=True)  # 判断EX文件存不存在，不存在就创建
+user_param = {'disc_lr': LEARNING_RATE_DIS, 'epochs': EPOCHS}  # 参数字典，包含了学习率以及本地训练轮次
 server_param = {}
 if MODE == "LDP":
     user_obj = LDPUser
@@ -82,10 +87,11 @@ elif MODE == "CDP":
     server_obj = CDPServer
     user_param['flr'] = LEARNING_RATE_F
     server_param['noise_multiplier'] = opacus.accountants.utils.get_noise_multiplier(target_epsilon=target_epsilon,
-                                                                                 target_delta=target_delta, 
-                                                                                 sample_rate=sample_rate, steps=ROUNDS)
+                                                                                     target_delta=target_delta,
+                                                                                     sample_rate=sample_rate,
+                                                                                     steps=ROUNDS)
     print(f"noise_multipier: {server_param['noise_multiplier']}")
-    server_param['sample_clients'] = sample_rate*NUM_CLIENTS
+    server_param['sample_clients'] = sample_rate * NUM_CLIENTS
 else:
     raise ValueError("Choose mode from [CDP, LDP]")
 
@@ -93,21 +99,24 @@ if DATA_NAME == 'purchase':
     root = 'data/purchase/dataset_purchase'
 elif DATA_NAME == 'chmnist':
     root = 'data/CHMNIST'
-else: root = '~/torch_data'
+else:
+    root = '~/torch_data'
 
-#数据集名字，目录，客户端数量，批次大小，每个客户端分配的标签数量，标签总数量
+# train_dataloader包含了每个客户端的train_dataloader,test_dataloaders同理，这里也可能用迪利克雷分布函数去控制每个客户端分配到的样本数量，原论文作者这样分配的好处可能是为了让训练的损失减少更稳定
 train_dataloaders, test_dataloaders = gen_random_loaders(DATA_NAME, root, NUM_CLIENTS,
+                                                         ##数据集名字，目录，客户端数量，批次大小，每个客户端分配的标签数量，标签总数量
                                                          BATCH_SIZE, NUM_CLASES_PER_CLIENT, NUM_CLASSES)
 
 print(user_param)
-users = [user_obj(i, device, MODEL, None, NUM_CLASSES, train_dataloaders[i], **user_param) for i in range(NUM_CLIENTS)]   #在函数调用中，**用于解包字典，将字典中的每个键值对作为关键字参数传递给函数。
+users = [user_obj(i, device, MODEL, None, NUM_CLASSES, train_dataloaders[i], **user_param) for i in
+         range(NUM_CLIENTS)]  # 在函数调用中，**用于解包字典，将字典中的每个键值对作为关键字参数传递给函数。
 server = server_obj(device, MODEL, None, NUM_CLASSES, **server_param)
 for i in range(NUM_CLIENTS):
     users[i].set_model_state_dict(server.get_model_state_dict())
 best_acc = 0
 for round in range(ROUNDS):
-    random_index = np.random.choice(NUM_CLIENTS, int(sample_rate*NUM_CLIENTS), replace=False)
-    for index in random_index:users[index].train()
+    random_index = np.random.choice(NUM_CLIENTS, int(sample_rate * NUM_CLIENTS), replace=False)
+    for index in random_index: users[index].train()
     if MODE == "LDP":
         weights_agg = agg_weights([users[index].get_model_state_dict() for index in random_index])
         for i in range(NUM_CLIENTS):
@@ -116,7 +125,7 @@ for round in range(ROUNDS):
         server.agg_updates([users[index].get_model_state_dict() for index in random_index])
         for i in range(NUM_CLIENTS):
             users[i].set_model_state_dict(server.get_model_state_dict())
-    print(f"Round: {round+1}")
+    print(f"Round: {round + 1}")
     acc = evaluate_global(users, test_dataloaders, range(NUM_CLIENTS))
     if acc > best_acc:
         best_acc = acc
@@ -127,12 +136,13 @@ for round in range(ROUNDS):
             break
 
 end_time = time.time()
-print("Use time: {:.2f}h".format((end_time - start_time)/3600.0))
+print("Use time: {:.2f}h".format((end_time - start_time) / 3600.0))
 print(f'Best accuracy: {best_acc}')
-results_df = pd.DataFrame(columns=["data","num_client","ncpc","mode","model","epsilon","accuracy"])
+results_df = pd.DataFrame(columns=["data", "num_client", "ncpc", "mode", "model", "epsilon", "accuracy"])
 results_df = results_df._append(
     {"data": DATA_NAME, "num_client": NUM_CLIENTS,
-     "ncpc": NUM_CLASES_PER_CLIENT, "mode":MODE,
+     "ncpc": NUM_CLASES_PER_CLIENT, "mode": MODE,
      "model": MODEL, "epsilon": target_epsilon, "accuracy": best_acc},
     ignore_index=True)
-results_df.to_csv(f'log/E{args.E}/{DATA_NAME}_{NUM_CLIENTS}_{NUM_CLASES_PER_CLIENT}_{MODE}_{MODEL}_{target_epsilon}.csv', index=False)
+results_df.to_csv(
+    f'log/E{args.E}/{DATA_NAME}_{NUM_CLIENTS}_{NUM_CLASES_PER_CLIENT}_{MODE}_{MODEL}_{target_epsilon}.csv', index=False)
--- datasets.py
+++ datasets.py
@@ -126,10 +126,10 @@ def gen_classes_per_node(dataset, num_users, classes_per_user=2, high_prob=0.6, 
     # -------------------------------------------#
     # print(num_classes)
     assert (classes_per_user * num_users) % num_classes == 0, "equal classes appearance is needed" #它检查每个客户端获得的类别数量是否能够整除总的类别数量。如果条件不满足，即无法等分类别给每个客户端，就会引发 AssertionError
-    count_per_class = (classes_per_user * num_users) // num_classes
+    count_per_class = (classes_per_user * num_users) // num_classes #计算每个类别应该被分配给多少个客户端
     class_dict = {}
     for i in range(num_classes):
-        probs=np.array([1]*count_per_class)
+        probs=np.array([1]*count_per_class)  #这个数组 probs 表示了当前类别被分配给不同客户端的概率。因为每个客户端应该平均分配类别，所以每个类别的概率都初始化为1
         probs_norm = (probs / probs.sum()).tolist()
         class_dict[i] = {'count': count_per_class, 'prob': probs_norm}
     # -------------------------------------#
@@ -139,11 +139,11 @@ def gen_classes_per_node(dataset, num_users, classes_per_user=2, high_prob=0.6, 
     for i in range(num_users):
         c = []
         for _ in range(classes_per_user):
-            class_counts = [class_dict[i]['count'] for i in range(num_classes)]
-            max_class_counts = np.where(np.array(class_counts) == max(class_counts))[0]
-            max_class_counts = np.setdiff1d(max_class_counts, np.array(c))
-            c.append(np.random.choice(max_class_counts))
-            class_dict[c[-1]]['count'] -= 1
+            class_counts = [class_dict[i]['count'] for i in range(num_classes)]   #其中包含了每个类别的剩余分配数量
+            max_class_counts = np.where(np.array(class_counts) == max(class_counts))[0]  #max_class_counts 是一个包含整数值的一维 NumPy 数组，每个整数值表示一个类别的索引，这些类别在剩余分配数量中具有最大值。这些索引可以用于后续的操作，比如随机选择一个类别分配给当前客户端，以确保数据的均匀分布。
+            max_class_counts = np.setdiff1d(max_class_counts, np.array(c)) #排除已经分配给当前客户端的类别，以确保不会重复分配。np.setdiff1d 函数用于找到两个数组的差集，从 max_class_counts 中去掉已经分配的类别。
+            c.append(np.random.choice(max_class_counts))  #从剩余最多的类的索引随机选一个
+            class_dict[c[-1]]['count'] -= 1  #将被分配的类别的剩余分配数量减去1，以反映已经分配给一个客户端
         class_partitions['class'].append(c)
         class_partitions['prob'].append([class_dict[i]['prob'].pop() for i in c])
     return class_partitions
@@ -163,12 +163,20 @@ def gen_data_split(dataset, num_users, class_partitions):
     # Create class index mapping #
     # -------------------------- #
     data_class_idx = {i: np.where(data_labels_list == i)[0] for i in range(num_classes)}
-
+    #这行代码创建了一个字典 data_class_idx，其中包含了每个类别的数据索引。
+    '''
+    data_class_idx = {
+        0: array([1, 3, 7, ...]),    # 类别0的样本索引
+        1: array([2, 5, 10, ...]),   # 类别1的样本索引
+        ...                       # 其他类别的样本索引
+    }
+    '''
     # --------- #
     # Shuffling #
     # --------- #
     for data_idx in data_class_idx.values():
         random.shuffle(data_idx)
+
 
     # ------------------------------ #
     # Assigning samples to each user #
@@ -176,9 +184,9 @@ def gen_data_split(dataset, num_users, class_partitions):
     user_data_idx = [[] for i in range(num_users)]
     for usr_i in range(num_users):
         for c, p in zip(class_partitions['class'][usr_i], class_partitions['prob'][usr_i]):
-            end_idx = int(num_samples[c] * p)
-            user_data_idx[usr_i].extend(data_class_idx[c][:end_idx])
-            data_class_idx[c] = data_class_idx[c][end_idx:]
+            end_idx = int(num_samples[c] * p)  #计算应该分配给当前客户端的类别 c 的样本数量，根据分配概率和总样本数量来确定。
+            user_data_idx[usr_i].extend(data_class_idx[c][:end_idx])  #将类别 c 的前 end_idx 个样本索引添加到当前客户端的 user_data_idx 列表中。
+            data_class_idx[c] = data_class_idx[c][end_idx:]   #从 data_class_idx 中移除已经分配的样本索引，以确保不会重复分配。
         if len(user_data_idx[usr_i])%2 == 1: user_data_idx[usr_i] = user_data_idx[usr_i][:-1]
 
     return user_data_idx
@@ -235,14 +243,20 @@ def gen_random_loaders(data_name, data_path, num_users, bz, num_classes_per_user
     for i, d in enumerate(datasets):  #i是索引，d是数据集对象
         if i == 0:
             cls_partitions = gen_classes_per_node(d, num_users, num_classes_per_user)
+            '''
+            class_partitions = {
+                'class': [c_1, c_2, ..., c_num_users],  # 每个 c_i 是一个列表，包含被分配给第 i 个客户端的类别
+                'prob': [p_1, p_2, ..., p_num_users]     # 每个 p_i 是一个列表，包含了第 i 个客户端类别的分配概率
+            }
+            '''
             print(cls_partitions)
             for index in range(num_users):
-                distribution[index][cls_partitions['class'][index]] = cls_partitions['prob'][index]
+                distribution[index][cls_partitions['class'][index]] = cls_partitions['prob'][index]  #此时每行包括了两个不为0的元素，元素的位置就是分配给客户端的类别，值就是相应的概率
 
             loader_params['shuffle'] = True
-        usr_subset_idx = gen_data_split(d, num_users, cls_partitions)
+        usr_subset_idx = gen_data_split(d, num_users, cls_partitions) #返回值是每个客户端被分配的样本索引
 
-        subsets = list(map(lambda x: torch.utils.data.Subset(d, x), usr_subset_idx))
-        dataloaders.append(list(map(lambda x: torch.utils.data.DataLoader(x, **loader_params), subsets)))
+        subsets = list(map(lambda x: torch.utils.data.Subset(d, x), usr_subset_idx))  #接下来，将每个客户端被分配的样本索引列表 usr_subset_idx 转化为 PyTorch 数据子集（Subset）对象。这将为每个客户端创建一个独立的数据子集，包含了他们分配到的样本。
+        dataloaders.append(list(map(lambda x: torch.utils.data.DataLoader(x, **loader_params), subsets))) #最后，将每个数据子集（Subset）使用 PyTorch 的 DataLoader 类转化为数据加载器。数据加载器用于按批次加载数据并进行训练
 
     return dataloaders
